c3313c6e91f36d85c7939265e96d6c60062b0e13
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-07-27 14:54:33 -0300

    feat(routes): implement responsive routing to components
    verify the screen size of client and then display the proper component
    
    BREAKING CHANGE: changes main component to be from mobile or desktop page

--- climao/src/app/app-routing.module.ts
+++ climao/src/app/app-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
+import { ApplicationStateService } from './services/application-state.service';
+import { DesktopPageComponent } from './pages/desktop-page/desktop-page.component';
+import { MobilePageComponent } from './pages/mobile-page/mobile-page.component';
 
 
-const routes: Routes = [];
+const desktopRoutes: Routes = [
+  {
+    path: '', component: DesktopPageComponent, children:
+    [
+      // {path: 'city/:cityId', component: ProductComponent}
+    ]
+  },
+  // directs all other routes to the main page
+  {path: '**', redirectTo: ''}
+];
+
+const mobileRoutes: Routes = [
+  {path: '', component: MobilePageComponent},
+  // {path: 'city/:cityId', component: ProductComponent}
+  // directs all other routes to the main page
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(desktopRoutes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor (private router: Router, private applicationState: ApplicationStateService) {
+    if (applicationState.getIsMobileResolution()) {
+      router.resetConfig(mobileRoutes);
+    }
+  }
+}
