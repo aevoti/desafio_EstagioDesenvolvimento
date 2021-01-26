ca412a3f2dbb2c8e1ed2fa7a38984c837aecb326
Guilherme Stein Kuhn <guilherme.steink@gmail.com>
2021-01-26 20:00:47 -0300

    Add files via upload

---
+++ apiweather/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+
+  title="apiweather"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
