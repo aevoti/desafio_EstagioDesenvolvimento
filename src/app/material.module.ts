9b7b35171da8e112af01c6b2bff5106795487072
hadamo <hadamosieg@gmail.com>
2021-01-29 16:33:27 -0300

    update material modules imports correction

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
-
 
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
