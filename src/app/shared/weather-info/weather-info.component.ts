e867e8299985f39c7f4b75c9402af739d3c8efb3
hadamo <hadamosieg@gmail.com>
2021-01-28 23:57:47 -0300

    dynamic icon for weather info component

--- src/app/shared/weather-info/weather-info.component.ts
+++ src/app/shared/weather-info/weather-info.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input } from '@angular/core';
+import {
+    Component,
+    ComponentFactoryResolver,
+    ViewChild,
+    ViewContainerRef,
+    Input,
+    OnInit,
+    ComponentRef,
+    ComponentFactory,
+    ElementRef
+} from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
 
 
 @Component({
@@ -6,11 +19,42 @@ import { Component, Input } from '@angular/core';
     templateUrl: './weather-info.component.html',
     styleUrls: ['./weather-info.component.scss']
 })
-export class WeatherInfoComponent {
+export class WeatherInfoComponent implements OnInit {
 
     @Input()
     name: string;
 
     @Input()
     value: string | number;
+
+    @Input()
+    icon: IconDefinition;
+
+    @ViewChild('host', {
+        read: ViewContainerRef, static: true
+    }) container: ViewContainerRef;
+
+    @ViewChild('info') weatherInfo: ElementRef
+
+
+    constructor(private cfr: ComponentFactoryResolver) {
+    }
+
+    ngOnInit(): void {
+        // this.createIcon();
+    }
+
+    ngAfterViewInit() {
+        if (this.weatherInfo) {
+            this.createIcon();
+        }
+    }
+
+    private createIcon(): void {
+        const factory: ComponentFactory<FaIconComponent> = this.cfr.resolveComponentFactory(FaIconComponent);
+        const componentRef: ComponentRef<FaIconComponent> = this.container.createComponent(factory);
+        componentRef.instance.icon = this.icon;
+        componentRef.instance.render();
+    }
+
 }
