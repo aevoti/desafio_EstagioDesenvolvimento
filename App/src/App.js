5eb84be501de3f657f823474f4efab21e57b79cf
Arthur Fusco <fuscoarthur1@gmail.com>
2021-02-02 20:41:56 -0300

    create - global.css

--- App/src/App.js
+++ App/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import './style.css';
+import './global.css';
 
 import SearchWeather from './components/SearchWeather';
 import LocationDate from './components/LocationDate';
