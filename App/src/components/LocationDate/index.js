564e785c2599c6c7556af851edbcfcbba0cb50f1
Arthur Fusco <fuscoarthur1@gmail.com>
2021-02-03 12:31:06 -0300

    format date

--- App/src/components/LocationDate/index.js
+++ App/src/components/LocationDate/index.js
@@ -4,12 +4,18 @@ import './style.css';
 
 function LocationDate(props) {
     const location = `${props.location.name}, ${props.location.region}, ${props.location.country}`;
+    const date = new Date(props.location.localtime);
     return (
         <div className={props.className}>
             <span className="location">{location}</span>
-            <span className="date">{props.location.localtime}</span>
+            <span className="date">{formatDate(date)}</span>
         </div>
     )
+}
+
+function formatDate(date) {
+    const newDate = date.toString().split(' ').slice(0, 5);
+    return newDate.join(' ');
 }
 
 export default LocationDate;
