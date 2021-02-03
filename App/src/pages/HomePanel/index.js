e6d2f6e51b50270400c78fd125efc3ed01b5a413
Arthur Fusco <fuscoarthur1@gmail.com>
2021-02-03 18:04:00 -0300

    style - HomePanel

--- App/src/pages/HomePanel/index.js
+++ App/src/pages/HomePanel/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import './style.css';
+
 function HomePanel(props) {
     return (
         <div className={props.className}>
             <h1>Welcome to Weather API</h1>
+            <h2>
+                Retrieve instant, accurate weather information for 
+                any location in the world
+            </h2>
         </div>
     )
 }
