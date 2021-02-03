cf49d3acbb99fc954311a8ff40fd43a2ddc54eff
Arthur Fusco <fuscoarthur1@gmail.com>
2021-02-02 21:38:11 -0300

    update - input style & button style

--- App/src/components/Button/index.js
+++ App/src/components/Button/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './style.css';
 
 function Button(props) {
-    return <button type={props.type} onClick={props.onClick}>Submit</button>
+    return <button type={props.type} onClick={props.onClick}>{props.text}</button>
 }
 
 export default Button;
--- App/src/components/SearchWeather/index.js
+++ App/src/components/SearchWeather/index.js
@@ -9,7 +9,7 @@ function SearchWeather(props) {
         <form className={props.className} onSubmit={props.onSubmit}>
             <Input value={props.city.name} onChange={props.city.handleCityInput} placeholder="City"/>
             <Input value={props.region.name} onChange={props.region.handleRegionInput} placeholder="Region" />
-            <Button type="submit"/>
+            <Button type="submit" text="Search"/>
         </form>
     )
 }
