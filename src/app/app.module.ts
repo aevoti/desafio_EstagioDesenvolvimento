1790828919d3f0e0e6b1f5fbc9bb42d0441299cb
Hádamo Egito <hadamoegito@hotmail.com>
2021-06-23 10:25:23 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherAppComponent } from './weather-app/weather-app.component';
