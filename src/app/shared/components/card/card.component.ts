a85798262e65aa33e2d44da33e09dd0ccfc9a7fa
Erikbernard <erikbrsilva@gmail.com>
2024-04-24 10:40:00 -0300

    feat: card component and shared module

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Pokemon } from 'src/app/core/models/pokemon.model';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  init: Pokemon = {
+    id: 0,
+    name: "",
+    gif: "",
+    stat: 0,
+    types: [
+      {
+        slot: 0,
+        type: { name: "" },
+      }
+    ]
+  }
+
+  @Input() pokemon: Pokemon = this.init;
+  @Output() onItemSelected = new EventEmitter<{ name: string, stat: number, gif: string }>();
+  selectPokemon(): void {
+    const { stat, gif, name } = this.pokemon;
+    this.onItemSelected.emit({ name, stat, gif });
+  }
+}
